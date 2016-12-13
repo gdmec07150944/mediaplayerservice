cf2cd2702e03045c8048192c397d56180e1a6be6
ygf44 <529110167@qq.com>
2016-12-13 17:40:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus1/myapplication/PlayService.java
@@ -0,0 +1,42 @@
+package com.example.asus1.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ASUS on 2016/11/20.
+ */
+public class PlayService extends Service {
+    MediaPlayer mediaPlayer;
+    public PlayService(){
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer=MediaPlayer.create(this,R.raw.exodus);
+        mediaPlayer.setLooping(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+}
